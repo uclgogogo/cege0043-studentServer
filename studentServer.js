8b2c6022fae3992a8689ae05c1a98820cf3b6ce4
uclgogogo <logan1234456@outlook.com>
2019-02-25 21:49:49 +0000

    add phone gap function

--- studentServer.js
+++ studentServer.js
@@ -21,6 +21,13 @@ console.log("The file " + filename + " was requested.");
 next();
 });
 
+// cross origin request (for phonegap)
+app.use(function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Headers", "X-Requested-With");
+next();
+});
+
 // serve static files - e.g. html, css
 // this should always be the last line in the server file
 app.use(express.static(__dirname));
